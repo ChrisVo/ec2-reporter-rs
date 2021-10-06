7f0c1630c3a9b4cb8492ceb98a68c78b2ccbfa9b
Chris Vo <cvo@rivian.com>
2021-10-05 17:17:13 -0700

    able to traverse results from ec2 client results

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,27 @@ use std::default::Default;
 
 #[tokio::main]
 async fn main() {
+    get_instances().await;
+}
+
+async fn get_instances() {
     let client = Ec2Client::new(Region::UsEast2);
     let ec2_instances_input: DescribeInstancesRequest = Default::default();
-    
-    match client.describe_instances(ec2_instances_input).await {
-        Ok(output) => println!("{:?}", output),
-        Err(error) => println!("{:?}", error)
+    // ec2_instances_input.max_results = Some(5);
+    let instances = client.describe_instances(ec2_instances_input).await;
+
+    match instances {
+        Ok(output) => {
+            for reservation in output.reservations.iter() {
+                for reservations in reservation.iter() {
+                    for instances in reservations.instances.iter() {
+                        for instance in instances.iter() {
+                            println!("{:?}", instance.instance_id)
+                        }
+                    }
+                }
+            }
+        }
+        Err(err) => println!("{:?}", err),
     }
 }
