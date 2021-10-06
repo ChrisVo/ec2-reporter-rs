f733a3ad0fd31ceafb21632eddb2544a32138d2f
Chris Vo <cvo@rivian.com>
2021-10-05 19:20:54 -0700

    added sts caller identity command to get account id

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 import boto3
 
 ec2 = boto3.client('ec2')
+sts = boto3.client('sts')
+account_id = sts.get_caller_identity()
 instances = ec2.describe_instances()
+print(f"\nInstances in AWS Account ID {account_id['Account']}")
 for reservation in instances['Reservations']:
     for instance in reservation['Instances']:
-        print(instance['InstanceId'])
+        print(f"\t- {instance['InstanceId']}")
+print("\n")
